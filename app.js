0170b77dfab5f736037f0d64b6ff85bd34576c9d
Jordan Johansen <jordanjohan@gmail.com>
2015-10-02 16:41:33 -0700

    Fiddle around with home page.

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var nicksflix = angular.module('nicksflix', ['ui.router']);
 
 nicksflix.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('home', {
+  $stateProvider.state('splash', {
     url: '',
     views: {
       'header': {
         templateUrl: 'partials/header.html',
       },
       'body': {
-        templateUrl: 'partials/home.html',
-        controller: 'ReviewCtrl'
+        templateUrl: 'partials/splash.html'
+      }
+    }
+  });
+
+  $stateProvider.state('home', {
+    url: 'home',
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: 'partials/home.html'
       }
     }
   });
